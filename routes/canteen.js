0261868e84a4eb2750f3abba1669cc9ab6671406
rong <darong6126@gmail.com>
2025-08-11 10:05:11 +0700

    add view dishes by categoty

--- routes/canteen.js
+++ routes/canteen.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {addDish, updateDish, getAllDishes,deleteDish} from '../controllers/canteen.js';
+import {addDish, updateDish, getAllDishes, deleteDish, getAllDishesByCategory} from '../controllers/canteen.js';
 import { authenticateToken } from '../middlewares/auth.js';
 import {upload} from '../middlewares/upload.js';
 import { authorizeRole } from '../middlewares/authorizeRole.js';
@@ -10,5 +10,6 @@ canteenRouter.post('/', authenticateToken,authorizeRole('staff'),upload.single('
 canteenRouter.put('/:id',authenticateToken,authorizeRole('staff'),upload.single('imageFile'),updateDish);
 canteenRouter.get('/',authenticateToken,authorizeRole('staff'),getAllDishes);
 canteenRouter.delete('/:id',authenticateToken,authorizeRole('staff'),deleteDish);
+canteenRouter.get('/category/:categoryId',authenticateToken,authorizeRole('staff'),getAllDishesByCategory);
 
 export default canteenRouter;
