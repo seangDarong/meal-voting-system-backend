2718432daf240d8a444d0b50ca7352547942e0dd
rong <darong6126@gmail.com>
2025-08-14 10:37:06 +0700

    add new votepoll feature

--- routes/canteen.js
+++ routes/canteen.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {submitVoteOptions} from '../controllers/canteen.js';
+import {submitVoteOptions , getActiveVotePoll} from '../controllers/canteen.js';
 import { authenticateToken } from '../middlewares/auth.js';
 import { authorizeRole } from '../middlewares/authorizeRole.js';
 
 const canteenRouter = express.Router();
 canteenRouter.post('/',authenticateToken,authorizeRole('staff'),submitVoteOptions);
+canteenRouter.get('/active',getActiveVotePoll);
 
 export default canteenRouter;
