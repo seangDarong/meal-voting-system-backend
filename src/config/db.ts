e03dfdd5ae07d00e407636c28c8f4a5f302f0a75
Sliden101 <chessplayz11@gmail.com>
2025-09-04 15:04:21 +0700

    fix: tf

--- src/config/db.ts
+++ src/config/db.ts
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         // port: process.env.DB_PORT || 5432,
-        port: port || 5432,
+        port: port || 30002,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT!! as 'postgres' | 'mysql' | 'sqlite' | 'mssql',
         logging: true, // optional
